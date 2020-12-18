1e6ade528dd19ca2c2ebd1ed882ef17fc985e296
David Taylor <david@taylorhq.com>
2020-12-18 00:04:35 +0000

    DEV: Refactor into component with computed properties
    This commit also
      - Removes use of api.onPageChange in favor of the router service
      - Removes JQuery use
      - Uses currentRouteName instead of currentURL so that the bar is hidden for loading substates

---
+++ javascripts/discourse/components/search-banner.js
@@ -0,0 +1,61 @@
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+import { and } from "@ember/object/computed";
+import discourseComputed, { observes } from "discourse-common/utils/decorators";
+
+export default Component.extend({
+  router: service(),
+  tagName: "",
+
+  @discourseComputed("router.currentRouteName")
+  displayForRoute(currentRouteName) {
+    const showOn = settings.show_on;
+    if (showOn == "homepage") {
+      return currentRouteName == `discovery.${defaultHomepage()}`;
+    } else if (showOn == "top_menu") {
+      return this.siteSettings.top_menu
+        .split("|")
+        .any((m) => `discovery.${m}` == currentRouteName);
+    } else {
+      // "all"
+      return (
+        currentRouteName != "full-page-search" &&
+        !currentRouteName.startsWith("admin.")
+      );
+    }
+  },
+
+  @discourseComputed("currentUser")
+  displayForUser() {
+    const showFor = settings.show_for;
+    if (showFor == "everyone") {
+      return true;
+    } else if (showFor == "logged_out" && !currentUser) {
+      return true;
+    } else if (showFor == "logged_in" && currentUser) {
+      return true;
+    }
+    return false;
+  },
+
+  shouldDisplay: and("displayForUser", "displayForRoute"),
+
+  // Setting a class on <html> from a component is not great
+  // but we need it for backwards compatibility
+  @observes("shouldDisplay")
+  displayChanged() {
+    document.documentElement.classList.toggle(
+      "display-search-banner",
+      this.shouldDisplay
+    );
+  },
+
+  didInsertElement() {
+    this.displayChanged();
+  },
+
+  didDestroyElement() {
+    document.documentElement.classList.remove("display-search-banner");
+  },
+});
