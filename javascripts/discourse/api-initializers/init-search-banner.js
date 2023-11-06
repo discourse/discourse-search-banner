e20a36d9f58763cc68eabb14e3c4a0737a67094a
Natalie Tay <natalie.tay@gmail.com>
2023-11-06 12:22:20 +0800

    DEV: Use new i18n package (#43)

--- javascripts/discourse/api-initializers/init-search-banner.js
+++ javascripts/discourse/api-initializers/init-search-banner.js
@@ -2,7 +2,7 @@ import { apiInitializer } from "discourse/lib/api";
 import { logSearchLinkClick } from "discourse/lib/search";
 import { iconNode } from "discourse-common/lib/icon-library";
 import { h } from "virtual-dom";
-import I18n from "I18n";
+import I18n from "discourse-i18n";
 import SearchBanner from "../components/search-banner";
 
 export default apiInitializer("1.14.0", (api) => {
