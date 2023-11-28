d57ce477f89e06aec3c42689645dbbea2cec597e
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2023-11-28 13:59:07 -0700

    DEV: Update to glimmer search menu (#46)
    # Without Search Button Text
    
    <img width="599" alt="Screenshot 2023-11-28 at 1 41 23 PM" src="https://github.com/discourse/discourse-search-banner/assets/50783505/f3db62c8-3a64-410a-a254-88cdab9d5d9f">
    <img width="635" alt="Screenshot 2023-11-28 at 1 41 32 PM" src="https://github.com/discourse/discourse-search-banner/assets/50783505/c595d6e3-bc04-472c-869a-9c2567399085">
    <img width="580" alt="Screenshot 2023-11-28 at 1 44 11 PM" src="https://github.com/discourse/discourse-search-banner/assets/50783505/400a9b72-c599-4518-94f7-a46eb41eb541">
    
    
    # With Search Button Text
    
    <img width="604" alt="Screenshot 2023-11-28 at 1 46 17 PM" src="https://github.com/discourse/discourse-search-banner/assets/50783505/1195e798-0d21-489f-8672-a5ccd3960b91">
    <img width="557" alt="Screenshot 2023-11-28 at 1 45 39 PM" src="https://github.com/discourse/discourse-search-banner/assets/50783505/7eb6681b-08f3-4a7f-98ba-8a3fbe283baa">
    
    
    # Shared search term
    https://github.com/discourse/discourse-search-banner/assets/50783505/fdf12c12-1c9a-4fb6-bd44-8ca5fe2d30d0

---
+++ test/javascripts/acceptance/search-banner-test.js
@@ -0,0 +1,73 @@
+import { acceptance, query } from "discourse/tests/helpers/qunit-helpers";
+import { click, fillIn, triggerKeyEvent, visit } from "@ember/test-helpers";
+import { test } from "qunit";
+
+acceptance("Discourse Search Banner", function (needs) {
+  needs.user({ admin: true, experimental_search_menu_groups_enabled: true });
+
+  test("Search Banner is present", async function (assert) {
+    await visit("/");
+    assert.dom(".custom-search-banner input#search-term").exists();
+  });
+
+  test("is only present on intended routes", async function (assert) {
+    await visit("/admin");
+    assert.dom(".custom-search-banner").doesNotExist();
+  });
+
+  test("it closes the search menu when clicking outside", async function (assert) {
+    await visit("/");
+    await click(".custom-search-banner input#search-term");
+    await fillIn(".custom-search-banner input#search-term", "test");
+    assert.dom(".custom-search-banner .results").exists();
+
+    // select a element to simulate clicking outside the search banner
+    await click(".custom-search-banner h1");
+    assert.dom(".custom-search-banner .results").doesNotExist();
+  });
+
+  test("pressing escape closes the search menu", async function (assert) {
+    await visit("/");
+    await click(".custom-search-banner input#search-term");
+    await fillIn(".custom-search-banner input#search-term", "test");
+    assert.dom(".custom-search-banner .results").exists();
+
+    await triggerKeyEvent(
+      ".custom-search-banner #search-term",
+      "keyup",
+      "Escape"
+    );
+    assert.dom(".custom-search-banner .results").doesNotExist();
+  });
+
+  test("searching for a term in the search menu fills in the search banner search input", async function (assert) {
+    await visit("/");
+    await click("#search-button");
+    await fillIn("#search-term", "test");
+    assert
+      .dom(".custom-search-banner input#search-term")
+      .hasValue("test", "search inputs have matching terms");
+  });
+
+  test("you can navigate search results with the keyboard", async function (assert) {
+    const container = ".custom-search-banner .results";
+
+    await visit("/");
+    await click(".custom-search-banner input#search-term");
+    await fillIn(".custom-search-banner input#search-term", "test");
+
+    await triggerKeyEvent(
+      ".custom-search-banner #search-term",
+      "keyup",
+      "Enter"
+    );
+    assert.dom(`${container} .search-result-topic`).exists("has topic results");
+
+    await triggerKeyEvent(document.activeElement, "keyup", "ArrowDown");
+    assert.strictEqual(
+      document.activeElement.getAttribute("href"),
+      query(`${container} li:first-child a`).getAttribute("href"),
+      "arrow down selects first element"
+    );
+  });
+});
