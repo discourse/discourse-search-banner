0e267b617cae1dd5501794dc379be0561cf0d600
Jordan Vidrine <30537603+jordanvidrine@users.noreply.github.com>
2024-03-06 17:34:55 -0600

    FIX: Remove outlet causing search icon injection elsewhere (#52)

--- spec/system/viewing_search_banner_spec.rb
+++ spec/system/viewing_search_banner_spec.rb
@@ -24,19 +24,6 @@ RSpec.describe "Viewing the search banner", type: :system do
     expect(page).to have_css("#main-outlet .custom-search-banner")
   end
 
-  it "should hide the search icon when searching within a topic" do
-    theme.update_setting(:show_on, "all")
-    theme.save!
-
-    topic_page.visit_topic(topic)
-    expect(topic_page).to have_css(".custom-search-banner")
-    topic_page.find(".custom-search-banner input#search-term").fill_in(with: "test")
-    topic_page.find(".custom-search-banner .results li:nth-child(2) a").click
-
-    expect(topic_page).to have_css(".custom-search-banner .search-context")
-    expect(topic_page).to_not have_css(".custom-search-banner .search-icon")
-  end
-
   it "should display the search icon when searching within a topic when search button text is present" do
     theme.update_setting(:show_on, "all")
     theme.update_translation("search_banner.search_button_text", "Foo")
