2a37fab737791ce41d815ec20d191f6b80bc05b6
David Taylor <david@taylorhq.com>
2024-03-27 17:32:10 +0000

    DEV: Update linting

--- javascripts/discourse/components/search-banner.js
+++ javascripts/discourse/components/search-banner.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import I18n from "discourse-i18n";
 
