1763ef4ed9ae0c867cea4abcdb12cfdc8953977a
Jarek Radosz <jarek@cvx.dev>
2025-10-08 22:23:22 +0200

    DEV: Update linting

--- javascripts/discourse/api-initializers/init-search-banner.js
+++ javascripts/discourse/api-initializers/init-search-banner.js
@@ -1,7 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 import SearchBanner from "../components/search-banner";
 
-export default apiInitializer("1.14.0", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet(
     settings.plugin_outlet === "above-main-container"
       ? "above-main-container"
